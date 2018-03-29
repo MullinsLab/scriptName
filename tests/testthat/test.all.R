fff2b796044c80c6d8e4d7e2b454d60a6b20e3e9
Thomas Sibley <trsibley@uw.edu>
2018-03-29 12:22:23 -0700

    Tests: Use fully qualified paths to the current R and Rscript
    PATH may not be set to point to the current R version, or even any R
    version at all.  This was revealed (although very obtusely, and only
    with much digging) via failures during rhub's automated testing on
    Fedora with R-devel.  It makes sense though, and is the equivalent of
    using $^X in Perl.

--- tests/testthat/test.all.R
+++ tests/testthat/test.all.R
@@ -1,9 +1,12 @@
 library(rlang)
 
+R       <- file.path(R.home("bin"), "R")
+Rscript <- file.path(R.home("bin"), "Rscript")
+
 context("source()")
 test_that("functions work under source()", {
     source_file <- function(file)
-       system2("Rscript", c("--vanilla", "-e", shQuote(paste0('source("', file, '")'))), stdout = T)
+       system2(Rscript, c("--vanilla", "-e", shQuote(paste0('source("', file, '")'))), stdout = T)
 
     expect_identical(source_file("current_filename.R"),         ' chr "current_filename.R"')
     expect_identical(source_file("current_source_filename.R"),  ' chr "current_source_filename.R"')
@@ -14,7 +17,7 @@ test_that("functions work under source()", {
 context("R --file")
 test_that("functions work under R --file", {
     run_r <- function(file)
-       system2("R", c("--slave", "--vanilla", "--file=red-herring.R", paste0("--file=", file)), stdout = T)
+       system2(R, c("--slave", "--vanilla", "--file=red-herring.R", paste0("--file=", file)), stdout = T)
 
     expect_identical(run_r("current_filename.R"),         ' chr "current_filename.R"')
     expect_identical(run_r("current_source_filename.R"),  ' NULL')
@@ -25,7 +28,7 @@ test_that("functions work under R --file", {
 context("R -f")
 test_that("functions work under R -f", {
     run_r <- function(file)
-       system2("R", c("--slave", "--vanilla", "-f", "red-herring.R", "-f", file), stdout = T)
+       system2(R, c("--slave", "--vanilla", "-f", "red-herring.R", "-f", file), stdout = T)
 
     expect_identical(run_r("current_filename.R"),         ' chr "current_filename.R"')
     expect_identical(run_r("current_source_filename.R"),  ' NULL')
@@ -36,7 +39,7 @@ test_that("functions work under R -f", {
 context("Rscript")
 test_that("functions work under Rscript", {
     run_rscript <- function(file)
-       system2("Rscript", c("--vanilla", file), stdout = T)
+       system2(Rscript, c("--vanilla", file), stdout = T)
 
     expect_identical(run_rscript("current_filename.R"),         ' chr "current_filename.R"')
     expect_identical(run_rscript("current_source_filename.R"),  ' NULL')
